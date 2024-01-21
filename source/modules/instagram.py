ca4e10de09e120e8435ae3ae6e7041ff3570348e
Aviax <87578989+CyberPixelPro@users.noreply.github.com>
2024-01-21 13:34:55 +0530

    Update instagram.py

--- source/modules/instagram.py
+++ source/modules/instagram.py
@@ -1,17 +1,16 @@
-import requests
+import httpx
+import os
 from bs4 import BeautifulSoup
 import re
-import os
 
-def modify_url(instagram_url):
+async def modify_url(instagram_url):
     return instagram_url.replace('www.instagram.com', 'www.ddinstagram.com')
 
-def extract_download_link(modified_url):
-    response = requests.get(modified_url)
+async def extract_download_link(client, modified_url):
+    response = await client.get(modified_url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         video_tag = soup.find('a', href=re.compile(r'(.mp4)$'))
-
         if video_tag and 'href' in video_tag.attrs:
             return video_tag['href']
         else:
@@ -19,19 +18,24 @@ def extract_download_link(modified_url):
     else:
         return None
 
-def download_video(video_url, destination_folder='/cache'):
-    local_filename = os.path.join(destination_folder, video_url.split('/')[-1])
-    with requests.get(video_url, stream=True) as r:
-        r.raise_for_status()
-        with open(local_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                f.write(chunk)
-    return local_filename
+async def download_video(client, video_url, destination_folder='/cache'):
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
 
-def download_reel(instagram_url):
-    modified_url = modify_url(instagram_url)
-    download_link = extract_download_link(modified_url)
-    if download_link:
-        return download_video(download_link)
+    local_filename = os.path.join(destination_folder, video_url.split('/')[-1])
+    response = await client.get(video_url)
+    if response.status_code == 200:
+        with open(local_filename, 'wb') as f:
+            f.write(response.content)
+        return local_filename
     else:
         return None
+
+async def download_reel(instagram_url):
+    async with httpx.AsyncClient() as client:
+        modified_url = await modify_url(instagram_url)
+        download_link = await extract_download_link(client, modified_url)
+        if download_link:
+            return await download_video(client, download_link)
+        else:
+            return None
